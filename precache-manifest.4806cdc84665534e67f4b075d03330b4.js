c6550f384df7f4b0d5e2bf3dd840ed55a192dc2a
hunter <hunterkeng@hotmail.com>
2019-03-12 11:23:25 +1300

    mv index to root folder

---
+++ precache-manifest.4806cdc84665534e67f4b075d03330b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3ff71c08731b4b0e82d2",
+    "url": "/static/js/main.673add0d.chunk.js"
+  },
+  {
+    "revision": "e49762ef1f5fb6363ba0",
+    "url": "/static/js/2.387ff043.chunk.js"
+  },
+  {
+    "revision": "49f9fdf315ff633d55bb857ed53f1788",
+    "url": "/index.html"
+  }
+];
